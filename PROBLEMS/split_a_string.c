1399f94afe62f81a6d55a64b4bdc644c91424e3e
Dhinakar S <dhinakarshanmugam6@gmail.com>
2024-03-14 18:40:05 +0530

    changed

--- PROBLEMS/split_a_string.c
+++ PROBLEMS/split_a_string.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    long number, product = 1, digit;
+    long number,count=0, product = 1, digit;
     number = 438344;
     printf("digits are: ");
     while (number > 0) {
+        count++;
         digit = number % 10;
         product *= digit;
         printf("%ld ", digit);
         number /= 10;
     }
     printf("\ndigits product is: %ld", product);
+    printf("\nnumber of digits: %ld", count);
     return 0;
 }
